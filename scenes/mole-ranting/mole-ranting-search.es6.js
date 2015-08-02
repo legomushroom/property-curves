9ef6f4cf987ffebfa117cc69ba0672fcc403dabe
Oleg Solomka <legomushroom@gmail.com>
2015-08-02 11:33:52 +0300

    search scene: add door move

--- scenes/mole-ranting/mole-ranting-search.es6.js
+++ scenes/mole-ranting/mole-ranting-search.es6.js
@@ -10,6 +10,10 @@ var moleRantingSearch = {
   },
   vars: function () {
     this.s = 1;
+
+    this.doorEl             = document.querySelector('#js-door');
+    this.doorShadowEl       = document.querySelector('#js-door-shadow');
+    this.doorHandleShadowEl = document.querySelector('#js-door-handle-shadow');
     // this.delay = (this.s < 5) ? 1400 : 0;
   },
   createTween: function () {
@@ -18,17 +22,21 @@ var moleRantingSearch = {
     var e = mojs.easing;
     // var handAngleCurve = e.path('M0,0.0557234763 L7.9453125,0.158137605 L7.9453125,-62.2096335 L27.0139179,-62.2096335 L27.0139179,66.661464 L45.9410019,66.661464 L45.9410012,-0.27805839 L45.9410019,-94.524739 L100.009766,-94.524739');
     var bodySkewC = e.path('M0.506500786,99.4107635 C0.506500786,99.4107635 5.85142524,99.4107621 7.9810887,99.4107621 C7.9810887,66.899765 18.5815826,-9.81235939 32.5697561,49.5905614 C34.3447271,56.1428608 40.36649,56.1939167 41.0047155,49.5905613 C42.965489,30.0062046 44.1572271,-22.244141 59.113501,-7.025553 C62.059815,-3.8203124 60.6154791,0.141451938 64.710198,0.141452075 C65.4163407,0.141452071 66.1289969,0.141452067 66.8462524,0.141452063 C82.5602506,0.141451987 100.481854,0.14145211 100.481854,0.14145211');
+    var doorAngleC = e.mix({ to: .5, value: 1 }, { to: 1, value: bodySkewC });
     var tween = new mojs.Tween({
       delay:    200*this.s,
       duration: 1300*this.s,
       onUpdate: (p)=> {
         
-        mojs.h.style(this.moleEl, 'transform', `skewX(${9*bodySkewC(p)}deg) scale(${1-.01*bodySkewC(p)}, ${1+.01*bodySkewC(p)}) translateX(${-7*bodySkewC(p)}px) rotateY(${-10*bodySkewC(p)}deg)`);
+        var bodySkewP = bodySkewC(p);
+        mojs.h.style(this.moleEl, 'transform', `skewX(${9*bodySkewP}deg) scale(${1-.01*bodySkewP}, ${1+.01*bodySkewP}) translateX(${-7*bodySkewP}px) rotateY(${-10*bodySkewP}deg)`);
+        mojs.h.style(this.moleHandEl, 'transform', `rotate(${33 + 60*bodySkewP}deg) scaleY(${1+.07*bodySkewP})`);
+        mojs.h.style(this.moleHandLeftEl, 'transform', `rotate(${20*bodySkewP}deg) translate(${24*bodySkewP}px, ${-10*bodySkewP}px)`);
+        
 
-        mojs.h.style(this.moleHandEl, 'transform', `rotate(${33 + 60*bodySkewC(p)}deg) scaleY(${1+.07*bodySkewC(p)})`);
-        mojs.h.style(this.moleHandLeftEl, 'transform', `rotate(${20*bodySkewC(p)}deg) translate(${24*bodySkewC(p)}px, ${-10*bodySkewC(p)}px)`);
-
-
+        var doorAngleP = doorAngleC(p);
+        mojs.h.setPrefixedStyle(this.doorEl, 'transform', `rotateY(${-125 + 40*e.inverse(doorAngleP)}deg) scaleX(.75) ${this.zHack}`);
+        mojs.h.setPrefixedStyle(this.doorShadowEl, 'transform', `scaleX(${1-.5*e.inverse(doorAngleP)}) ${this.zHack}`);
       }
     });
 
